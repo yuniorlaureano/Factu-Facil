4df4d7256c0863598dd54e72fc5c242a06cd5802
Yunior Laureano <yu.laureano@aduanas.gob.do>
2021-04-12 22:16:31 -0400

    Inicializando proyecto Factu-Facil

---
+++ Factu-Facil/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Factu_Facil.Models;
+
+namespace Factu_Facil.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
