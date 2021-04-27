0a674da1fa80840fab6bd9b457e8afab1d23010f
Yunior Laureano <yu.laureano@aduanas.gob.do>
2021-04-27 12:58:28 -0400

    renombrando el proyecto

--- Factu-Facil/Views/AboutPageText.xaml.cs
+++ Factu-Facil/Views/AboutPageText.xaml.cs
@@ -3,14 +3,14 @@ using System.ComponentModel;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace Factu_Facil.Views
+namespace FactuFacil.Views
 {
     // Learn more about making custom code visible in the Xamarin.Forms previewer
     // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
-    public partial class AboutPageTest : ContentPage
+    public partial class AboutPageText : ContentPage
     {
-        public AboutPageTest()
+        public AboutPageText()
         {
             InitializeComponent();
         }
